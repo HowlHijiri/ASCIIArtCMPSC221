d3f4ae59e25e41b98d06cf837014638fd4891193
Bret Castrinoes <42556324+HowlHijiri@users.noreply.github.com>
2018-08-23 09:52:36 -0400

    Add files via upload

---
+++ bretACIIArt.java
@@ -0,0 +1,22 @@
+public class Main 
+{
+  
+   public static void main(String[] args) 
+   {
+    System.out.println("      _______________________                                 ");
+    System.out.println("     | ______________________|");
+    System.out.println("     ||                     ||");
+    System.out.println("     ||      CMPSC 221      ||");
+    System.out.println("     ||     ------------    ||");
+    System.out.println("     ||      ASCIIArt       ||");
+    System.out.println("     ||      ( ͡° ͜ʖ ͡ )       ||   ");
+    System.out.println("     ||_____________________||");
+    System.out.println("     |_________\\    /________|");
+    System.out.println("              _|____|_******   *");
+    System.out.println("          _________________ *** ** ");
+    System.out.println("         /----------------/    ___*     ");
+    System.out.println("        /----------------/    ////     ");
+    System.out.println("       /----------------/    /__/      ");
+  }
+  
+}
